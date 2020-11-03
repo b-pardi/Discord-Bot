c4fb49cde9f096246514b5834638930718d02b25
brandonator24 <72064522+brandonator24@users.noreply.github.com>
2020-11-02 20:33:35 -0800

    Barebones bot

---
+++ dbot.py
@@ -0,0 +1,16 @@
+from imports import *
+
+#link to manage bot
+#https://discord.com/developers/applications/773035122982387712/information
+
+#establishes client for bot
+client = commands.Bot(command_prefix = '.')
+
+#create first event, readies the bot for use
+@client.event
+async def on_ready():
+    print("Bot ready to go")
+
+#run bot from token, DO NOT SHARE TOKEN
+client.run(token)
+time.sleep(5)
---
+++ imports.py
@@ -0,0 +1,6 @@
+import os
+import time
+import discord 
+from discord.ext import commands
+
+from notatoken import *
